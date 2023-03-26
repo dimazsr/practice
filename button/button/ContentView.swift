c45ccbea2eabde3efb550e057ff91f5b5ad6637b
dimazsr <127843799+dimazsr@users.noreply.github.com>
2023-03-27 00:04:55 +0900

    버튼 +- 연습

--- button/button/ContentView.swift
+++ button/button/ContentView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            
+            Button(action: {
+                number = number-1
+                print("minus 1")
+            }, label: {
+                Text("-1")
+                    .foregroundColor(.primary)
+                    .padding(20)
+                    .background(Color.white)
+                    .clipShape(Circle())
+                    .frame(width: 150, height: 150)
+                    .shadow(color: .gray, radius: 3, x: 0, y: 2)
+                    .padding()
+            })
+            
+           Text("\(number)")
+                .frame(width: 30.0)
+            
+            Button(action: {
+                number = number+1
+                print("plus 1")
+            }, label: {
+                Text("+1")
+                    .foregroundColor(.primary)
+                    .padding(20)
+                    .background(Color.white)
+                    .clipShape(Circle())
+                    .frame(width: 150, height: 150)
+                    .shadow(color: .gray, radius: 3, x: 0, y: 2)
+                    .padding()
+            })
         }
-        .padding()
     }
 }
 
+// Content View를 보여주는 코드
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
