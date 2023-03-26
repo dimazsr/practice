436a601f33999b564ec9a95903ec25109cf3c2fe
dimazsr <127843799+dimazsr@users.noreply.github.com>
2023-03-26 23:48:59 +0900

    git 커밋 연습

---
+++ button/button/buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  buttonApp.swift
+//  button
+//
+//  Created by 안수린 on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
